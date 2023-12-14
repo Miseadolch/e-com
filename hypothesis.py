130989e7e8a003cacd4816018f41c0e5712a2131
TerBee4 <118851255+TerBee4@users.noreply.github.com>
2023-12-14 21:15:20 +0300

    Update hypothesis.py

--- hypothesis.py
+++ hypothesis.py
@@ -11,22 +11,22 @@ region_device = df.groupby(['region', 'device'])['payer'].sum()
 if ((shapiro(days_purchases)[1] >= 0.05) and (shapiro(region_device)[1] >= 0.05)):
     r = round(f_oneway(days_purchases, region_device)[1], 4)
     #print(r)
-    # p-value меньше 0.05 - взаимосвязь между типом устройства и кол-вом покупок в день есть
+    # p-value меньше 0.01 - взаимосвязь между типом устройства и кол-вом покупок в день есть
 else:
     k = round(kruskal(days_purchases, region_device)[1], 4)
     print(k)
-    # p-value меньше 0.05 - взаимосвязь между типом устройства и кол-вом покупок в день есть
+    # p-value меньше 0.01 - взаимосвязь между типом устройства и кол-вом покупок в день есть
 
 
 region_channel = df.groupby(['region', 'channel'])['payer'].sum()
 if ((shapiro(days_purchases)[1] >= 0.05) and (shapiro(region_channel)[1] >= 0.05)):
     r = round(f_oneway(days_purchases, region_channel)[1], 4)
     #print(r)
-    # p-value меньше 0.05 - взаимосвязь между рекламным каналом и кол-вом покупок в день есть
+    # p-value меньше 0.01 - взаимосвязь между рекламным каналом и кол-вом покупок в день есть
 else:
     k = round(kruskal(days_purchases, region_channel)[1], 4)
     print(k)
-    # p-value меньше 0.05 - взаимосвязь между рекламным каналом и кол-вом покупок в день есть
+    # p-value меньше 0.01 - взаимосвязь между рекламным каналом и кол-вом покупок в день есть
 
 
 
